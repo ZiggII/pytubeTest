904169e1836620e6ad50c9d1f4cb1740c7d49fa5
ZiggII <zdeno@sekerak.eu>
2022-11-21 20:09:15 +0100

    change

--- pytubeTest.py
+++ pytubeTest.py
@@ -1,7 +1,6 @@
-import pytube
 from pytube import YouTube
 
-myVideo = YouTube("https://www.youtube.com/watch?v=Ctqi5Y4X-jA")
+myVideo = YouTube(input("Enter the video link: "))
 
 
 stream = myVideo.streams
@@ -9,7 +8,15 @@ stream = myVideo.streams
 for stream in myVideo.streams:
     print(f"{stream.filesize/1024/1024}--{stream.resolution}--{stream.itag}")
 
-downloadITag = input("Now choose the video and write the iTag: ")    
+downloadITag = "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+printAgain = True
+while printAgain == True and downloadITag != "exit":
+    try:
+        downloadITag = input("Now choose the video and write the iTag: ")    
 
-whatToDownload = myVideo.streams.get_by_itag(downloadITag)
-whatToDownload.download
+        whatToDownload = myVideo.streams.get_by_itag(downloadITag)
+        whatToDownload.download
+        printAgain = False
+    except:
+        print("Try again")
+        printAgain = True
