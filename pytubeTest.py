167395976a7d8ed5952fc04dc603cbbc4b263cf6
ZiggII <zdeno@sekerak.eu>
2022-11-21 19:30:59 +0100

    change

--- pytubeTest.py
+++ pytubeTest.py
@@ -5,10 +5,11 @@ myVideo = YouTube("https://www.youtube.com/watch?v=Ctqi5Y4X-jA")
 
 
 stream = myVideo.streams
-#itagOfStream = stream.
-#print(stream.)
 
 for stream in myVideo.streams:
     print(f"{stream.filesize/1024/1024}--{stream.resolution}--{stream.itag}")
 
-downloadITag = input("NOw choose the video and write the iTag: ")    
+downloadITag = input("Now choose the video and write the iTag: ")    
+
+whatToDownload = myVideo.streams.get_by_itag(downloadITag)
+whatToDownload.download
