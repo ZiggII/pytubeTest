3cb35ff580c1f48ab0dc363588281ea3242246dd
ZiggII <zdeno@sekerak.eu>
2022-11-21 19:21:52 +0100

    initialize

---
+++ pytubeTest.py
@@ -0,0 +1,13 @@
+import pytube
+from pytube import YouTube
+
+myVideo = YouTube("https://www.youtube.com/watch?v=Ctqi5Y4X-jA")
+
+
+stream = myVideo.streams
+#itagOfStream = stream.
+#print(stream.)
+
+for stream in myVideo.streams:
+    print(f"{stream.filesize/1024/1024}--{stream.resolution}")
+    
