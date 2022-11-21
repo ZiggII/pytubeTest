44e531de6ea89e1b43163653cd1c0e7554f42607
ZiggII <zdeno@sekerak.eu>
2022-11-21 20:16:16 +0100

    change

--- pytubeTest.py
+++ pytubeTest.py
@@ -8,13 +8,14 @@ stream = myVideo.streams
 for stream in myVideo.streams:
     print(f"{stream.filesize/1024/1024}--{stream.resolution}--{stream.itag}")
 
-downloadITag = "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"
+downloadITag = None
 printAgain = True
 while printAgain == True and downloadITag != "exit":
     try:
         downloadITag = input("Now choose the video and write the iTag: ")    
 
         whatToDownload = myVideo.streams.get_by_itag(downloadITag)
+        #find a way to specify output path
         whatToDownload.download(output_path="/storage/emulated/0/Video")
         printAgain = False
     except:
