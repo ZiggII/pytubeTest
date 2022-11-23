cccb76194c8ae18f0bd643ef63e9c20a758dd41d
ZiggII <zdeno@sekerak.eu>
2022-11-23 18:47:00 +0100

    change

--- pytubeTest.py
+++ pytubeTest.py
@@ -4,6 +4,11 @@ myVideo = YouTube(input("Enter the video link: "))
 
 stream = myVideo.streams
 
+try:
+    opened_file = open("pathFile.txt", "r")
+except FileNotFoundError:
+    print("Fail, try creating a path file, for that restart application and select the option...")
+
 for stream in myVideo.streams:
     print(f"{stream.filesize/1024/1024}--{stream.resolution}--{stream.itag}")
 
@@ -15,7 +20,7 @@ while printAgain == True and downloadITag != "exit":
 
         whatToDownload = myVideo.streams.get_by_itag(downloadITag)
         #find a way to specify output path
-        whatToDownload.download(output_path="/storage/emulated/0/Video")
+        whatToDownload.download(output_path=opened_file.read())
         printAgain = False
     except:
         print("Try again")
