175f88e0e98824f060b0645e15f039185773fe67
ZiggII <zdeno@sekerak.eu>
2022-11-30 18:28:00 +0100

    change

--- pytubeTest.py
+++ pytubeTest.py
@@ -43,6 +43,7 @@ stream = myVideo.streams
 
 for stream in myVideo.streams:
     print(f"{stream.filesize/1024/1024}--{stream.resolution}--{stream.itag}--{stream.method}")
+    
 
 downloadITag = None
 printAgain = True
