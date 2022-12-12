7c09ccac7c88dbbf7de81dabdae949dc750f6184
ZiggII <zdeno@sekerak.eu>
2022-12-12 19:50:16 +0100

    chnage

--- pytubeTest.py
+++ pytubeTest.py
@@ -75,8 +75,8 @@ if whatToDownload.video_codec is None:
         else:
             print("Invalid input, try again")
     if convertToMp3 == "y":
-        print(opened_file.read)
-        path = Path(opened_file.read) /  myVideo.title + ".mp4"
+        print(opened_file.read())
+        path = Path(opened_file.read()) /  myVideo.title + ".mp4"
         path.rename(path.with_suffix('.mp3'))
         
 opened_file.close()
